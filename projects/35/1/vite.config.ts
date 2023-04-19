56b3b85ad4dceb34c42e247fa2bb3ce6360967c8
Alexander Babich <alexander.taurus@gmail.com>
2023-04-19 10:25:53 +0300

    Update vite.config.ts

--- projects/35/1/vite.config.ts
+++ projects/35/1/vite.config.ts
@@ -4,7 +4,7 @@ import { BASE_LINK } from './src/constants/quiz';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: BASE_LINK,
+  base: /se-practice.pp.ua/,
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   plugins: [react()],
 });
