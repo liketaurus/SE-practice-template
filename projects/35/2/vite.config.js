aee1a2427d947a88bd711ab5f407c815d5bdb0b9
Alexander Babich <alexander.taurus@gmail.com>
2023-04-19 10:33:17 +0300

    Update vite.config.js

--- projects/35/2/vite.config.js
+++ projects/35/2/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/se-practice.pp.ua/projects/35/2/',
+  base: 'se-practice.pp.ua/projects/35/2/',
   plugins: [vue()],
   base: '/Practice/'
 })
