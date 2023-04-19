9456656eb2068846faa188999b09c5b39f2589c1
Alexander Babich <alexander.taurus@gmail.com>
2023-04-19 10:46:12 +0300

    projects deleted

--- projects/35/2/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: 'se-practice.pp.ua/projects/35/2/',
-  plugins: [vue()],
-  base: '/Practice/'
-})
