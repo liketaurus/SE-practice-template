115653d617e8bd7fdc4c3e23d53c4cf4c634cc15
Kindinov Ivan <135726796+333B3@users.noreply.github.com>
2024-03-18 16:47:08 +0200

    C

---
+++ src/Cyberpunk2069/Scripts/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+func _process(delta):
+	velocity.x = 0
+	velocity.y = 0
+	var speed = 150
+	
+	if Input.is_key_pressed(KEY_SHIFT):
+		speed += 200
+	
+	if Input.is_key_pressed(KEY_W):
+		velocity.y = -speed
+	if Input.is_key_pressed(KEY_S):
+		velocity.y = speed
+	if Input.is_key_pressed(KEY_A):
+		velocity.x = -speed
+	if Input.is_key_pressed(KEY_D):
+		velocity.x = speed
+	
+	if velocity.x == 0 and velocity.y == 0:
+		$AnimationPlayer.play("idle")
+	elif speed == 150:
+		$AnimationPlayer.play("walk")
+	else:
+		$AnimationPlayer.play("run")
+
+	move_and_slide()
---
+++ src/Cyberpunk2069/Scripts/settings.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+
+
