34955910fe1112807e8d7dcac878391cf1e0f189
Жеревчук Сергій <Foxtrotserega@gmail.com>
2024-03-18 22:16:44 +0100

    Created map

--- src/Cyberpunk2069/Scripts/menu.gd
+++ src/Cyberpunk2069/Scripts/menu.gd
@@ -4,7 +4,7 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/yep.tscn")
 
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
