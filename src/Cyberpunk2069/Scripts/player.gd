32bdc13d11748637d11378a596da5b7a7e80e162
Жеревчук Сергій <Foxtrotserega@gmail.com>
2024-03-18 22:19:45 +0100

    Refactor

--- src/Cyberpunk2069/Scripts/player.gd
+++ src/Cyberpunk2069/Scripts/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-var paused = false
 
 func _process(delta):
 	velocity.x = 0
 	velocity.y = 0
+	
 	var speed = 150
 	
 	if Input.is_key_pressed(KEY_SHIFT):
@@ -15,8 +15,10 @@ func _process(delta):
 	if Input.is_key_pressed(KEY_S):
 		velocity.y = speed
 	if Input.is_key_pressed(KEY_A):
+		$Player.flip_h = true
 		velocity.x = -speed
 	if Input.is_key_pressed(KEY_D):
+		$Player.flip_h = false
 		velocity.x = speed
 	
 	if velocity.x == 0 and velocity.y == 0:
@@ -25,22 +27,5 @@ func _process(delta):
 		$AnimationPlayer.play("walk")
 	else:
 		$AnimationPlayer.play("run")
-	if Input.is_key_pressed(KEY_D):
-		$Player.flip_h = false
-	if Input.is_key_pressed(KEY_A):
-		$Player.flip_h = true
 
 	move_and_slide()
-
-	if Input.is_action_pressed("pause"):
-		pause()
-
-func pause():
-	if paused:
-		$Pause.hide()
-		Engine.time_scale = 1
-	else:
-		$Pause.show()
-		Engine.time_scale = 0
-
-	paused = !paused
