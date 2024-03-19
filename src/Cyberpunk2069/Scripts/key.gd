191f15cfe9bc9f199b6b0dabbb1941573e458baf
Artem Liftiev <artem.liftiev37@gmail.com>
2024-03-19 15:02:36 +0200

    Key

---
+++ src/Cyberpunk2069/Scripts/key.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var player_in_area = false
+
+func _ready() -> void:
+	connect("body_entered", _on_body_entered)
+	connect("body_exited", _on_body_exited)
+
+func _process(delta):
+	if Input.is_key_pressed(KEY_E) and player_in_area:
+		$Label.visible = false
+
+func _on_body_entered(body: Node) -> void:
+	player_in_area = true
+
+func _on_body_exited(body: Node) -> void:
+	player_in_area = false
