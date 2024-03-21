41f938a2ee0a8beb7da222d4d187f17fd4805aa8
Artem Liftiev <artem.liftiev37@gmail.com>
2024-03-21 21:48:16 +0200

    BetterFight

--- src/Cyberpunk2069/Scripts/Enemi.gd
+++ src/Cyberpunk2069/Scripts/Enemi.gd
@@ -5,36 +5,50 @@ var player_in_area = false
 var player_auch = false
 var player_fight = false
 var is_dead = 0
+var hp = 3
 var speed = 100
 var d = 0
 @onready var animation = $AnimatedSprite2D
 
 var timer
+var timer2
 
 func _process(delta):
 	_progress(delta)
 
-	if Input.is_key_pressed(KEY_SPACE) and player_fight:
-		d += 1
-		if d == 1:
-			is_dead = 1
-			animation.play("death")
-			speed = 0
-		elif d > 1:
-			speed = 0
-	elif player_in_area:
+	if hp == 2:
+		$Hp3.visible = false
+	elif hp == 1:
+		$Hp2.visible = false
+	elif hp == 0:
+		$Hp.visible = false
+		is_dead = 1
+		animation.play("death")
+		speed = 0
+
+	if player_in_area and is_dead == 0:
 		_move(delta)
 
 func _init():
 	timer = Timer.new()
 	add_child(timer)
 	timer.autostart = true
-	timer.wait_time = 3.0
+	timer.wait_time = 2.0
 	timer.connect("timeout", self.timeout)
+	
+	timer2 = Timer.new()
+	add_child(timer2)
+	timer2.autostart = true
+	timer2.wait_time = 0.5
+	timer2.connect("timeout", self.timeout2)
 
 func timeout():
 	if player_auch and is_dead < 1:
 		Global.hp -= 1
+
+func timeout2():
+	if Input.is_key_pressed(KEY_SPACE) and player_fight:
+		hp -= 1
 
 func _progress(delta):
 	if velocity == Vector2.ZERO and d < 1:
