04152c504ef2afcf08322d0df43d242697478ee1
Жеревчук Сергій <Foxtrotserega@gmail.com>
2024-03-26 12:45:14 +0100

    Fixed background music

--- src/Cyberpunk2069/Scripts/yep.gd
+++ src/Cyberpunk2069/Scripts/yep.gd
@@ -7,7 +7,8 @@ func _ready():
 	Global.enemy1 = $Sprites/Enemi
 	Global.enemy2 = $Sprites/Enemi2
 	Global.enemy3 = $Sprites/Enemi3
-
+	
+	$BackrgoundMusic.playing = Global.enable_music
 
 func _process(delta):
 	pass
