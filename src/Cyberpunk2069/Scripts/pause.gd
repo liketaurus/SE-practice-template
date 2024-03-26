e8c50a444511635608e560f6410c5e59721c07b0
Жеревчук Сергій <Foxtrotserega@gmail.com>
2024-03-26 13:50:25 +0100

    Yep

--- src/Cyberpunk2069/Scripts/pause.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _on_continued_pressed():
-	Engine.time_scale = 1
-	visible = false
-	
-func _on_end_pressed():
-	Engine.time_scale = 1
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
